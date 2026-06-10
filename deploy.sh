8c3eb4dddd36bf3323aeaa0989571b4a2cbc9f1a
Aritro <aritroroy1999@gmail.com>
2026-06-10 04:03:49 -0400

    c

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,28 @@ set -e
 
 REMOTE_USER="root"
 REMOTE_HOST="167.172.153.128"
+REMOTE_REPO="/root/server/chess-eng"
 REMOTE_PATH="/opt/chess/chess"
 REMOTE_ENV="/opt/chess/.env"
-BINARY="target/release/chess"
 ENV_FILE=".env"
 
-echo "==> Building release..."
-cargo build --release
-
-echo "==> Copying binary and .env to server..."
-scp "$BINARY" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+echo "==> Copying .env to server..."
 scp "$ENV_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_ENV"
 
-echo "==> Restarting service..."
-ssh "$REMOTE_USER@$REMOTE_HOST" "systemctl restart chess && systemctl status chess --no-pager"
+echo "==> Pushing code to server..."
+rsync -az --exclude target --exclude .git \
+    ./ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_REPO/"
+
+echo "==> Building on server..."
+ssh "$REMOTE_USER@$REMOTE_HOST" "cd $REMOTE_REPO && cargo build --release"
+
+echo "==> Stopping service..."
+ssh "$REMOTE_USER@$REMOTE_HOST" "systemctl stop chess"
+
+echo "==> Installing binary..."
+ssh "$REMOTE_USER@$REMOTE_HOST" "cp $REMOTE_REPO/target/release/chess $REMOTE_PATH && chmod +x $REMOTE_PATH"
+
+echo "==> Starting service..."
+ssh "$REMOTE_USER@$REMOTE_HOST" "systemctl start chess && systemctl status chess --no-pager"
 
 echo "==> Done. Live at https://chess.aritro.me"
